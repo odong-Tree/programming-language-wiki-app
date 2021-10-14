c38fc5512805745a74be084d14de2ae03b34bed7
odong-Tree <odongnamu.dev@gmail.com>
2021-10-14 16:43:31 +0900

    feat: DetailViewController Accessibility 설정 코드 추가

--- ProgrammingLanguageWiki/DetailViewController.swift
+++ ProgrammingLanguageWiki/DetailViewController.swift
@@ -6,9 +6,10 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-    @IBOutlet weak var languageTitleLabel: UILabel!
-    @IBOutlet weak var languageImageView: UIImageView!
-    @IBOutlet weak var languageDescriptionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var isLikeButton: UIButton!
     
     var programmingLanguage: ProgrammingLanguageInfo?
@@ -16,24 +17,43 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setAllData()
+        configureAccessibility()
     }
     
     private func setAllData() {
         guard let item = programmingLanguage else { return }
-        languageTitleLabel.text = item.name
-        languageImageView.image = item.logoImage
-        languageDescriptionLabel.text = item.body
+        nameLabel.text = item.name
+        logoImageView.image = item.logoImage
+        descriptionLabel.text = item.body
         updateLikeButton(item: item)
     }
     
     private func updateLikeButton(item: ProgrammingLanguageInfo) {
         isLikeButton.isSelected = item.isLike
+        isLikeButton.accessibilityHint = isLikeButton.isSelected ? "선택됨" : "즐겨찾기에 추가하기"
     }
     
     private func programmingLanguageIndex() -> Int? {
         guard let programmingLanguage = self.programmingLanguage else { return nil }
         let indexPath = ProgrammingLanguageInfoManager.shared.infoList.firstIndex(of: programmingLanguage)
         return indexPath
+    }
+    
+    private func configureAccessibility() {
+        guard let item = programmingLanguage else { return }
+        
+        backButton.accessibilityLabel = "뒤로 가기"
+        backButton.accessibilityIdentifier = "DetailViewCtonroller.backButton"
+        
+        isLikeButton.accessibilityLabel = "즐겨찾기"
+        isLikeButton.accessibilityIdentifier = "DetailViewController.isLikeButton"
+        
+        logoImageView.isAccessibilityElement = true
+        logoImageView.accessibilityLabel = "\(item.name) 로고"
+        logoImageView.accessibilityIdentifier = "DetailViewController.logoImageView"
+        
+        nameLabel.accessibilityIdentifier = "DetailViewController.nameLabel"
+        descriptionLabel.accessibilityIdentifier = "DetailViewController.descriptionLabel"
     }
     
     @IBAction func backButton(_ sender: Any) {
